6b173535b9dd84240606d19b713303892183bcaa
Tanin Pewluangsawat <70503441+TaninDean@users.noreply.github.com>
2023-05-14 19:44:30 +0700

    [ Gb-77 ] Add register page and login with email and password (#5)
    * Add the base login and register page
    
    * add style to input block
    
    * Add method to handler login and register
    
    * add package sha1 to check compromised password
    
    * Fix wrong from label
    
    * Fix bug if compromised password the account still created
    
    * fix: Correct the function call syntax
    
    * fix: Check the password and confirm password match
    
    * fix: Update the wording for username to be name-surname
    
    ---------
    
    Co-authored-by: Siratee K <sirateek@gmail.com>

---
+++ src/views/Register.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="registerBox">
+        <md-card class="registerCard">
+            <md-card-media-actions>
+                <md-card-media>
+                    <img src="@/assets/images/garnbarn-logo.png" class="garnbarnLogo" />
+                </md-card-media>
+
+                <md-card-actions>
+                    <h3>Register to GarnBarn</h3>
+                    <div>
+                        <form @submit.prevent="register">
+                            <div class="user">
+                                <label for="username">Name - Surname:</label>
+                                <input id="username" v-model="username" type="text" />
+                            </div>
+                            <div class="user">
+                                <label for="username">Email:</label>
+                                <input id="username" v-model="email" type="text" />
+                            </div>
+                            <div class="pass">
+                                <label for="password">Password:</label>
+                                <input id="password" v-model="password" type="password" />
+                            </div>
+                            <div class="con-pass">
+                                <label for="confirmPassword">Confirm Password:</label>
+                                <input id="confirmPassword" v-model="confirmPassword" type="password" />
+                            </div>
+                            <div>
+                                <button type="submit">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </md-card-actions>
+            </md-card-media-actions>
+        </md-card>
+    </div>
+</template>
+
+<script lang="ts">
+import firebase from "firebase";
+import { Vue, Component } from 'vue-property-decorator';
+import sha1 from 'sha1';
+
+@Component
+export default class Register extends Vue {
+    username = '';
+    password = '';
+    confirmPassword = '';
+    email = ''
+
+    checkCompromisedPassword(): boolean {
+        const hashedPassword: string = sha1(this.password);
+        // todo send api 
+        return true
+    }
+
+    register(): void {
+        if (!this.checkCompromisedPassword()) {
+            alert('Your passoword is compromised')
+            return
+        }
+
+        if (this.password !== this.confirmPassword) {
+            alert('Password and Confirm Password missmatched')
+            return
+        }
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.email, this.password)
+            .then((res) => {
+                res.user!
+                    .updateProfile({
+                        displayName: this.username
+                    })
+                    .then(() => {
+                        this.$router.push('/signin')
+                    });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    }
+}
+</script>
+
+<style scoped>
+.registerBox {
+    padding-top: 5%;
+    padding-top: 5%;
+}
+
+.registerCard {
+    padding: 10px;
+    padding-top: 15px;
+    border-radius: 20px;
+    background-color: #f9f9f9;
+    display: inline-block;
+    vertical-align: top;
+    overflow: auto;
+    border: 1px solid rgba(#000, 0.12);
+}
+
+.garnbarnLogo {
+    width: 150px;
+    height: auto;
+}
+
+hr.rounded {
+    margin-top: 5%;
+    margin-bottom: 5%;
+    margin-left: 10%;
+    margin-right: 10%;
+    border-top: 2px solid #bbb;
+    border-radius: 5px;
+}
+
+.ruser,
+.pass,
+.con-pass {
+    margin: 0.25rem;
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -8,6 +8,32 @@
 
         <md-card-actions>
           <h3>Sign in to GarnBarn</h3>
+          <div>
+            <form @submit.prevent="login">
+              <div class="auth">
+                <div>
+                  <label for="username">Email:</label>
+                  <input id="username" v-model="email" type="text" />
+                </div>
+                <div class="password">
+                  <label for="password">Password:</label>
+                  <input id="password" v-model="password" type="password" />
+                </div>
+              </div>
+              <div class="register-link">
+                <div>Don't have an account?<router-link to="/register">Register</router-link></div>
+              </div>
+              <div class="forget-password">
+                <router-link to="/forgot">Forgot your password?</router-link>
+              </div>
+              <div>
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          </div>
+          <div class="or">
+            <p>or</p>
+          </div>
           <section id="firebaseui-auth-container"></section>
         </md-card-actions>
       </md-card-media-actions>
@@ -23,6 +49,9 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class SignIn extends Vue {
+  email = '';
+  password = '';
+
   mounted(): void {
     let firebaseAuthInstance: firebase.auth.Auth = firebase.auth();
     let uiConfig: any = {
@@ -32,6 +61,18 @@ export default class SignIn extends Vue {
     };
     let ui = new firebaseUi.auth.AuthUI(firebaseAuthInstance);
     ui.start("#firebaseui-auth-container", uiConfig);
+  }
+
+  login(): void {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.email, this.password)
+      .then(() => {
+        this.$router.push('/home')
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   }
 }
 </script>
@@ -65,5 +106,24 @@ hr.rounded {
   margin-right: 10%;
   border-top: 2px solid #bbb;
   border-radius: 5px;
+}
+
+.forget-password {
+  text-align: right;
+  margin-bottom: 0.25rem;
+}
+
+.password {
+  margin: 0.25rem;
+}
+
+p {
+  margin-block-start: 1rem;
+  margin-block-end: 0em;
+}
+
+.or {
+  font-size: 1.5rem;
+  font-weight: 500;
 }
 </style>
