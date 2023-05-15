ac429232ae58aa9404f719c1ed0cb93bb6cf313c
TaninDean <reiodean.dean@gmail.com>
2023-05-15 12:28:23 +0700

    Change form format

--- src/views/ConsentForm.vue
+++ src/views/ConsentForm.vue
@@ -1,47 +1,41 @@
 <template>
     <div class="consentForm">
       <h2>Consent Form</h2>
-      <div class="consentText">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor
-          aliquam leo, id efficitur dui. Nulla fringilla neque vel metus
-          ullamcorper consequat. Suspendisse eu convallis elit. Morbi vestibulum
-          lacus sit amet nulla iaculis, et vestibulum elit faucibus. In fermentum
-          sapien a nibh interdum, eu consequat metus aliquam. Aenean posuere sit
-          amet lectus ut luctus. Fusce in varius ex.
-        </p>
-      </div>
-      <div class="consentCheckbox">
-        <label for="consent">
-          <input id="consent" type="checkbox" v-model="consentChecked" />
-          I agree to the terms and conditions
-        </label>
-        <label for="unaccept">
-          <input id="unaccept" type="checkbox" v-model="unacceptChecked" />
-          I do not accept the terms and conditions
-        </label>
-      </div>
+      <md-table>
+        <md-table-row v-for="(consent, index) in consents" :key="index">
+          <md-table-cell>{{ consent.detail }}</md-table-cell>
+          <md-table-cell>
+            <md-checkbox v-model="consent.checked" @input="validateForm" />
+          </md-table-cell>
+        </md-table-row>
+      </md-table>
       <div class="submitButton">
-        <button :disabled="!consentChecked || unacceptChecked" @click="submitForm">Submit</button>
+        <button :disabled="!isValidForm" @click="submitForm">Submit</button>
       </div>
     </div>
   </template>
   
-  <script lang="ts">
+  <script>
   import { Vue, Component } from 'vue-property-decorator';
   
   @Component
   export default class SignIn extends Vue {
-    consentChecked = false;
-    unacceptChecked = false;
+    consents = [
+      { detail: 'Consent 1', checked: false },
+      { detail: 'Consent 2', checked: false },
+      { detail: 'Consent 3', checked: false },
+    ];
+  
+    get isValidForm() {
+      return this.consents.every(consent => consent.checked);
+    }
   
     submitForm() {
-      if (this.unacceptChecked) {
-        alert('You must accept the terms and conditions to proceed.');
-      } else {
-        // Perform form submission logic here
-        alert('Form submitted!');
+      if (!this.isValidForm) {
+        alert('Please accept all consents to proceed.');
+        return;
       }
+      alert('Form submitted!');
     }
   }
   </script>
@@ -51,10 +45,6 @@
     max-width: 600px;
     margin: 0 auto;
     padding: 20px;
-  }
-  
-  .consentCheckbox {
-    margin-top: 10px;
   }
   
   .submitButton {
