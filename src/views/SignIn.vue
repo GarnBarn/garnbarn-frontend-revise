e7d62a4a9f834f8f56b5f6ec5fec7ed9e5e82ff6
Siratee K <sirateek@gmail.com>
2023-05-15 22:55:01 +0700

    feat(wip): Add OAuth Signin

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -21,10 +21,16 @@
                 </div>
               </div>
               <div class="register-link">
-                <div>Don't have an account?<router-link to="/register">Register</router-link></div>
+                <div>
+                  Don't have an account?<router-link to="/register"
+                    >Register</router-link
+                  >
+                </div>
               </div>
               <div class="forget-password">
-                <router-link to="/forgot-password">Forgot your password?</router-link>
+                <router-link to="/forgot-password"
+                  >Forgot your password?</router-link
+                >
               </div>
               <div>
                 <button type="submit">Login</button>
@@ -34,6 +40,11 @@
           <div class="or">
             <p>or</p>
           </div>
+          <div>
+            <md-button class="md-dense md-raised" @click="oidcLogin"
+              >Signin with OIDC Provider</md-button
+            >
+          </div>
           <section id="firebaseui-auth-container"></section>
         </md-card-actions>
       </md-card-media-actions>
@@ -42,88 +53,151 @@
 </template>
 
 <script lang="ts">
-import firebase from "firebase/app";
-import * as firebaseUi from "firebaseui";
-import "firebaseui/dist/firebaseui.css";
-import { Component, Vue } from "vue-property-decorator";
+  import firebase from "firebase/app";
+  import * as firebaseUi from "firebaseui";
+  import "firebaseui/dist/firebaseui.css";
+  import { Component, Vue } from "vue-property-decorator";
 
-@Component
-export default class SignIn extends Vue {
-  email = '';
-  password = '';
+  @Component
+  export default class SignIn extends Vue {
+    email = "";
+    password = "";
 
-  mounted(): void {
-    let firebaseAuthInstance: firebase.auth.Auth = firebase.auth();
-    let uiConfig: any = {
-      signInSuccessUrl: "/consent",
-      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-      signInFlow: "popup",
-    };
-    let ui = new firebaseUi.auth.AuthUI(firebaseAuthInstance);
-    ui.start("#firebaseui-auth-container", uiConfig);
-  }
+    mounted(): void {
+      let firebaseAuthInstance: firebase.auth.Auth = firebase.auth();
+      let uiConfig: any = {
+        signInSuccessUrl: "/consent",
+        signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+        signInFlow: "popup",
+      };
+      let ui = new firebaseUi.auth.AuthUI(firebaseAuthInstance);
+      ui.start("#firebaseui-auth-container", uiConfig);
 
-  login(): void {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(this.email, this.password)
-      .then(() => {
-        this.$router.push('/consent')
-      })
-      .catch((error) => {
-        alert(error.message);
+      // firebase
+      //   .auth()
+      //   .getRedirectResult()
+      //   .then((result) => {
+      //     // IdP data available in result.additionalUserInfo.profile.
+      //     // ...
+
+      //     console.log(result);
+      //     /** @type {firebase.auth.OAuthCredential} */
+      //     var credential = result.credential;
+
+      //     // // OAuth access and id tokens can also be retrieved:
+      //     // var accessToken = credential?.accessToken;
+      //     // var idToken = credential?.idToken;
+
+      //     console.log(result.credential?.toJSON());
+      //   })
+      //   .catch((error) => {
+      //     // Handle error.
+      //   });
+    }
+
+    login(): void {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.push("/consent");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    }
+
+    parseJwt(token: string): any {
+      try {
+        return JSON.parse(Buffer.from(token, "base64").toString());
+      } catch (e) {
+        return null;
+      }
+    }
+    oidcLogin(): void {
+      var provider = new firebase.auth.OAuthProvider("oidc.auth0");
+      provider.addScope("read:garnbarn");
+      provider.setCustomParameters({
+        redirect_uri: "https://google.com",
       });
+
+      firebase.auth().signInWithRedirect(provider);
+
+      // firebase
+      //   .auth()
+      //   .signInWithPopup(provider)
+      //   .then((result) => {
+      //     const oauthAccessToken = (result.credential?.toJSON() as any)
+      //       .oauthAccessToken;
+
+      //     console.log(oauthAccessToken);
+
+      //     if (!oauthAccessToken) {
+      //       alert("Signin error");
+      //       return;
+      //     }
+
+      //     const parsedJwt = this.parseJwt(oauthAccessToken);
+
+      //     console.log(parsedJwt);
+
+      //     console.log(parsedJwt.scope);
+      //     this.$router.push("/consent");
+      //   })
+      //   .catch((error) => {
+      //     alert(error.message);
+      //   });
+    }
   }
-}
 </script>
 
 <style scoped>
-.signInBox {
-  padding-top: 5%;
-  padding-top: 5%;
-}
+  .signInBox {
+    padding-top: 5%;
+    padding-top: 5%;
+  }
 
-.signInCard {
-  padding: 10px;
-  padding-top: 15px;
-  border-radius: 20px;
-  background-color: #f9f9f9;
-  display: inline-block;
-  vertical-align: top;
-  overflow: auto;
-  border: 1px solid rgba(#000, 0.12);
-}
+  .signInCard {
+    padding: 10px;
+    padding-top: 15px;
+    border-radius: 20px;
+    background-color: #f9f9f9;
+    display: inline-block;
+    vertical-align: top;
+    overflow: auto;
+    border: 1px solid rgba(#000, 0.12);
+  }
 
-.garnbarnLogo {
-  width: 150px;
-  height: auto;
-}
+  .garnbarnLogo {
+    width: 150px;
+    height: auto;
+  }
 
-hr.rounded {
-  margin-top: 5%;
-  margin-bottom: 5%;
-  margin-left: 10%;
-  margin-right: 10%;
-  border-top: 2px solid #bbb;
-  border-radius: 5px;
-}
+  hr.rounded {
+    margin-top: 5%;
+    margin-bottom: 5%;
+    margin-left: 10%;
+    margin-right: 10%;
+    border-top: 2px solid #bbb;
+    border-radius: 5px;
+  }
 
-.forget-password {
-  text-align: right;
-  margin-bottom: 0.25rem;
-}
+  .forget-password {
+    text-align: right;
+    margin-bottom: 0.25rem;
+  }
 
-.password {
-  margin: 0.25rem;
-}
+  .password {
+    margin: 0.25rem;
+  }
 
-p {
-  margin-block-start: 1rem;
-  margin-block-end: 0em;
-}
+  p {
+    margin-block-start: 1rem;
+    margin-block-end: 0em;
+  }
 
-.or {
-  font-size: 1.5rem;
-  font-weight: 500;
-}
+  .or {
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
 </style>
